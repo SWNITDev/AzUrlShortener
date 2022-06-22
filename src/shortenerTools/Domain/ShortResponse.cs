ef3378a6a6078665cc8bb8ea944bd079c09e9389
nklsss <98391492+nklsss@users.noreply.github.com>
2022-06-22 09:03:34 +0200

    Update ShortResponse.cs

--- src/shortenerTools/Domain/ShortResponse.cs
+++ src/shortenerTools/Domain/ShortResponse.cs
@@ -7,7 +7,8 @@ namespace Cloud5mins.domain
         public string ShortUrl { get; set; }
         public string LongUrl { get; set; }
         public string Title { get; set; }        
-
+        public string ErstelltVon{ get; set; }
+        
         public ShortResponse(){}
         public ShortResponse (string host, string longUrl, string endUrl, string title)
         {
